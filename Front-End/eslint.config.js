da83e015561144729e22bdee00c1b9b035d701c3
Alexander-Guacan <adguacan@espe.edu.ec>
2025-01-04 15:02:02 -0500

    Config eslint and add prettier

--- Front-End/eslint.config.js
+++ Front-End/eslint.config.js
@@ -1,27 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import pluginTailwind from "eslint-plugin-tailwindcss";
-import prettier from "eslint-config-prettier";
-import pluginPrettier from "eslint-plugin-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginTailwind from 'eslint-plugin-tailwindcss';
+import prettierConfig from 'eslint-config-prettier';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').FlatConfig[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+
+    languageOptions: {
+      globals: globals.browser,
+    },
+
+    plugins: {
+      tailwindcss: pluginTailwind,
+      react: pluginReact,
+    },
+
     rules: {
-      "prettier/prettier": "error", // Forzar reglas de Prettier
-      "tailwindcss/classnames-order": "warn", // Ordenar las clases de Tailwind
-      "tailwindcss/enforces-shorthand": "warn", // Aplicar abreviaturas
-      "tailwindcss/no-custom-classname": "off", // Permitir clases personalizadas
+      ...prettierConfig.rules,
+
+      ...pluginTailwind.configs.recommended.rules,
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/enforces-shorthand': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
+
+      ...pluginReact.configs['jsx-runtime'].rules,
     },
   },
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginTailwind.configs.recommended,
-  prettier,
-  pluginPrettier.configs.recommended,
 ];
