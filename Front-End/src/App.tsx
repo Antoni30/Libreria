38eeb89dd31fbd61374325b048b7e1e7b04f6bbf
Alexander-Guacan <adguacan@espe.edu.ec>
2025-01-04 16:02:22 -0500

    Add react router

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">¡Hola Mundo!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
