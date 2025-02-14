1e9b33d3c1f083ec74bc3c9ccbe5dfb302a3e700
Alexander-Guacan <adguacan@espe.edu.ec>
2025-02-13 21:05:45 -0500

    Fix lint and tailwind configs

--- Front-End/eslint.config.js
+++ Front-End/eslint.config.js
@@ -1,37 +1,41 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import pluginTailwind from 'eslint-plugin-tailwindcss';
-import prettierConfig from 'eslint-config-prettier';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 
-/** @type {import('eslint').FlatConfig[]} */
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-
+    settings: { react: { version: '18.3' } },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-
     plugins: {
-      tailwindcss: pluginTailwind,
-      react: pluginReact,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react,
     },
-
     rules: {
-      ...prettierConfig.rules,
-
-      ...pluginTailwind.configs.recommended.rules,
-      'tailwindcss/classnames-order': 'warn',
-      'tailwindcss/enforces-shorthand': 'warn',
-      'tailwindcss/no-custom-classname': 'off',
-
-      ...pluginReact.configs.recommended.rules,
-      ...pluginReact.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
     },
-  },
-
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  }
+)
