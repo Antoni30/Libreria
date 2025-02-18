ac8fdbb845431091b2c41d3855d76ce8d6d17fd9
Alexander-Guacan <adguacan@espe.edu.ec>
2025-02-18 02:32:09 -0500

    Add address validation

--- Front-End/src/shared/utils/validations.util.ts
+++ Front-End/src/shared/utils/validations.util.ts
@@ -1,6 +1,11 @@
 export function isValidEmail(email: string) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
+
 export function isValidPhoneNumber(phoneNumber: string) {
   return /^\d{10}$/.test(phoneNumber)
+}
+
+export function isValidAddress(address: string) {
+  return /^[A-Za-z0-9\s,.-]{5,100}$/.test(address)
 }
