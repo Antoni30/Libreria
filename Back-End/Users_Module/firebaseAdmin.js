f3bfb778977422b0c0ce5436133c1e55e30d5217
Alexander-Guacan <adguacan@espe.edu.ec>
2025-02-25 20:56:15 -0500

    Restore assert clause

--- Back-End/Users_Module/firebaseAdmin.js
+++ Back-End/Users_Module/firebaseAdmin.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  credential: admin.credential.cert(serviceAccount),
+});
 
 export const auth = admin.auth();
